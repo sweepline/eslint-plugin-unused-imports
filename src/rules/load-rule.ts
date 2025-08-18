4140e03f64f8c78de3dcdcb796d27c03da11e266
Anthony Fu <github@antfu.me>
2025-08-18 17:15:08 +0900

    chore: update deps

--- src/rules/load-rule.ts
+++ src/rules/load-rule.ts
@@ -7,9 +7,7 @@ const require = createRequire(import.meta.url);
 
 export function getBaseRule(): Rule.RuleModule {
     if (!rule) {
-        rule = getRuleFromTSLintPlugin()
-            ?? getRuleFromTSLint()
-            ?? getESLintBaseRule();
+        rule = getRuleFromTSLintPlugin() ?? getRuleFromTSLint() ?? getESLintBaseRule();
     }
     return rule!;
 }
@@ -19,13 +17,13 @@ export function getRuleFromTSLintPlugin() {
         const tslintPlugin = require("@typescript-eslint/eslint-plugin");
         return tslintPlugin.rules["no-unused-vars"];
     } catch (_) {
-        return null
+        return null;
     }
 }
 
 export function getRuleFromTSLint() {
     try {
-        const tslint = require('typescript-eslint')
+        const tslint = require("typescript-eslint");
         return tslint.plugin.rules["no-unused-vars"];
     } catch (_) {
         return null;
