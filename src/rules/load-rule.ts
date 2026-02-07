cd429bb474f941b7d6616116a3e3632bf21c6d9a
SukkaW <isukkaw@gmail.com>
2026-02-07 17:34:18 +0800

    fix(#117): ESLint 10.0.0 support

--- src/rules/load-rule.ts
+++ src/rules/load-rule.ts
@@ -31,6 +31,22 @@ export function getRuleFromTSLint() {
 }
 
 export function getESLintBaseRule() {
-    const eslint = require("eslint");
-    return new eslint.Linter({ configType: "eslintrc" }).getRules().get("no-unused-vars");
+    try {
+        const eslint = require("eslint");
+        return new eslint.Linter({ configType: "eslintrc" }).getRules().get("no-unused-vars");
+    } catch {
+        // since ESLint 10.0.0, new Linter({ configType: "eslintrc" }) will now throw an TypeError explicitly:
+        // https://github.com/eslint/eslint/blob/b69cfb32a16c5d5e9986390d484fae1d21e406f9/lib/linter/linter.js#L757
+        //
+        // This means we can catch the error and load the rule from an unsafe API:
+        const eslint_USE_AT_YOUR_OWN_RISK = require("eslint/use-at-your-own-risk");
+        // builtinRules was added since ESLint 8.0.0 and was mentioned in ESLint's "Migrate to ESLint 8.0.0" guide:
+        // https://eslint.org/docs/latest/use/migrate-to-8.0.0#-the-cliengine-class-has-been-removed
+        //
+        // However, it's still considered an unstable API and may change without a major version bump. We need to guard the access.
+        if ('builtinRules' in eslint_USE_AT_YOUR_OWN_RISK && eslint_USE_AT_YOUR_OWN_RISK.builtinRules instanceof Map) {
+            return eslint_USE_AT_YOUR_OWN_RISK.builtinRules.get("no-unused-vars");
+        }
+        throw new TypeError("[eslint-plugin-unused-imports] Cannot load 'no-unused-vars' rule from ESLint. This is most likely due to a breaking change in ESLint's internal API. Please report this issue to 'eslint-plugin-unused-imports'.");
+    }
 }
